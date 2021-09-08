cd5f6e3453321b03c49c2a78b9d4a839bed78cb6
Vihaan <87717779+Thunder-V@users.noreply.github.com>
2021-09-08 21:08:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+menu_list_array = ["Veg Margherita Pizza", " Neapolitan Pizza", " Veggie Pizza", " Pepperoni Pizza"];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        htmldata="menu_list_array.sort();"
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
